5be24fb814244dec289eb8450c050907b9263209
Ageu Manuel <140927063+metra174@users.noreply.github.com>
2026-03-25 23:21:17 +0100

    Update WhatsApp link in SOCIAL_LINKS constant
    T5

--- constants.tsx
+++ constants.tsx
@@ -150,5 +150,6 @@ export const GALLERY_IMAGES = [
 export const SOCIAL_LINKS = {
   instagram: 'https://www.instagram.com/avaeventos.s',
   facebook: 'https://www.facebook.com/share/1FdLjBMALU/',
-  whatsapp: `https://wa.me/${WHATSAPP_CONTACT}`
-} as const;
+  whatsapp: 'https://wa.me/244948757808'
+};
+
