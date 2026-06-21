d0ff1dca31d5173fe14e0546e999b4eff884d361
Ageu Manuel <140927063+metra174@users.noreply.github.com>
2026-06-21 05:23:21 +0100

    T2
    OK

--- constants.tsx
+++ constants.tsx
@@ -135,4 +135,54 @@ export const PACKAGES: Package[] = [
   }
 ];
 
-export const HERO_IMAGES =
+export const HERO_IMAGES = [
+  'https://i.imgur.com/bUoyiWU.png',
+  'https://i.imgur.com/3seNSZj.png',
+  'https://i.imgur.com/Q2EJEbg.png'
+];
+
+export const GALLERY_IMAGES = [
+  { url: 'https://i.imgur.com/N9i13N5.png', category: 'Casamentos', title: 'Decoração Pacote Label - Luxo Clássico' },
+  { url: 'https://i.imgur.com/3NKWiT8.png', category: 'Casamentos', title: 'Decoração Pacote Label - Mesa Principal' },
+  { url: 'https://i.imgur.com/UUcSNVV.png', category: 'Casamentos', title: 'Decoração Pacote Label - Cenário de Entrada' },
+  { url: 'https://i.imgur.com/frvFT54.png', category: 'Festas', title: 'Decoração Pacote Label - Ambientação' },
+  { url: 'https://i.imgur.com/tpUXVl9.png', category: 'Festas', title: 'Decoração Pacote Label - Iluminação Cênica' },
+  { url: 'https://i.imgur.com/3n3gaTp.png', category: 'Especiais', title: 'Decoração Pacote Label - Passarela de Cristal' },
+  { url: 'https://i.imgur.com/voamzPN.png', category: 'Casamentos', title: 'Decoração Pacote Label - Detalhes Florais' },
+  { url: 'https://i.imgur.com/0qSPOOO.png', category: 'Casamentos', title: 'Decoração Pacote Label - Cerimônia Premium' },
+  { url: 'https://i.imgur.com/KerTmNA.png', category: 'Especiais', title: 'Decoração Pacote Label - Lounge Clássico' },
+  { url: 'https://i.imgur.com/rMAVKle.png', category: 'Festas', title: 'Decoração Pacote Label - Detalhes de Brilho' },
+  { url: 'https://i.imgur.com/cGXKynd.png', category: 'Casamentos', title: 'Decoração Pacote Label - Gazebo dos Noivos' },
+  { url: 'https://i.imgur.com/FtlEMV2.png', category: 'Especiais', title: 'Decoração Pacote Label - Mesa Redonda' },
+  { url: 'https://i.imgur.com/1Wn9Pot.png', category: 'Festas', title: 'Decoração Pacote Label - Lounge Minimalista' },
+  { url: 'https://i.imgur.com/cjPUAe5.png', category: 'Gastronomia', title: 'Banquete & Buffet Estilo Ava' },
+
+  { url: 'https://i.imgur.com/XMykzEf.png', category: 'Casamentos', title: 'Decoração Clássica de Casamento' },
+  { url: 'https://i.imgur.com/nLxHquT.png', category: 'Especiais', title: 'Cenário de Entrada Monumental' },
+  { url: 'https://i.imgur.com/LmutJhO.png', category: 'Corporativo', title: 'Detalhes em Cristais e Ouro' },
+  { url: 'https://i.imgur.com/uqMWmbx.png', category: 'Casamentos', title: 'Ambiente de Recepção Premium' },
+  { url: 'https://i.imgur.com/qM3JIw0.png', category: 'Corporativo', title: 'Banquete de Gala' },
+  { url: 'https://i.imgur.com/U1FRqX5.png', category: 'Casamentos', title: 'Teto Floral de Luxo' },
+  
+  { url: 'https://i.imgur.com/kWySv7R.png', category: 'Especiais', title: 'Arte em Detalhes' },
+  { url: 'https://i.imgur.com/sinppBQ.png', category: 'Casamentos', title: 'Cerimonial de Luxo' },
+  { url: 'https://i.imgur.com/LfzqD2U.png', category: 'Festas', title: 'Ambiente Festivo Ava' },
+  { url: 'https://i.imgur.com/mrkRvmo.png', category: 'Especiais', title: 'Curadoria de Cenários' },
+  { url: 'https://i.imgur.com/51XbGRs.png', category: 'Casamentos', title: 'Altar Monumental' },
+  { url: 'https://i.imgur.com/ukMjTPt.png', category: 'Corporativo', title: 'Recepção Executiva' },
+  { url: 'https://i.imgur.com/wY7xj4F.png', category: 'Festas', title: 'Noite de Gala Ava' },
+  
+  { url: 'https://i.imgur.com/tZNLdZc.png', category: 'Gastronomia', title: 'Apresentação de Pratos Quentes' },
+  { url: 'https://i.imgur.com/ilVfZFK.png', category: 'Gastronomia', title: 'Buffet de Doces & Sobremesas' },
+  { url: 'https://i.imgur.com/VYPtZXG.png', category: 'Gastronomia', title: 'Entradas Gourmet Ava' },
+  { url: 'https://i.imgur.com/xo2cSQz.png', category: 'Gastronomia', title: 'Serviço de Banquetes Luxo' }, // CORREÇÃO: Linha duplicada removida aqui!
+
+  { url: 'https://i.imgur.com/scHAorL.png', category: 'Especiais', title: 'Arte Visual Ava' },
+  { url: 'https://i.imgur.com/HLBViFX.png', category: 'Corporativo', title: 'Espaço de Eventos' }
+];
+
+export const SOCIAL_LINKS = {
+  instagram: 'https://www.instagram.com/avaeventos.s',
+  facebook: 'https://www.facebook.com/share/1FdLjBMALU/',
+  whatsapp: 'https://wa.me/244948757808'
+};
