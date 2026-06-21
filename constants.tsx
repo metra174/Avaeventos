6d0003ffb0daa170abb55e00503f7424d2a38062
Ageu Manuel <140927063+metra174@users.noreply.github.com>
2026-06-21 11:17:04 +0100

    Revise event features and clean up image list
    Updated features and removed some image entries.

--- constants.tsx
+++ constants.tsx
@@ -1,4 +1,3 @@
-
 import { Package } from './types';
 
 export const ANGOLA_PROVINCES = [
@@ -127,10 +126,10 @@ export const PACKAGES: Package[] = [
     image: 'https://i.imgur.com/ukMjTPt.png',
     location: 'Benfica – Rua da Oficina da Bosch',
     features: [
-      'Aluguer do salão base inclúe: Música, Luzes, DJ, Suíte para os noivos, Cozinha equipada, Estacionamento, Cubas, Louça de apoio ao buffet frio',
-      'Especificação opcional (com Decoração): 25.000,00 AKZ por pessoa',
-      'Especificação opcional (com Decoração & Buffet sem bebida): 45.000,00 AKZ por pessoa',
-      'Especificação opcional (com Decoração, Buffet, Bebida com álcool e sem álcool): 65.000,00 AKZ por pessoa'
+      'Aluguer do salão base inclui: Música, Luzes de pista, DJ exclusivo, Suíte para os noivos, Cozinha equipada e Estacionamento privativo seguro',
+      'Opção com Decoração: + 25.000,00 AKZ por pessoa',
+      'Opção com Decoração & Buffet (sem bebida): + 45.000,00 AKZ por pessoa',
+      'Opção com Decoração, Buffet & Bebida: + 65.000,00 AKZ por pessoa'
     ]
   }
 ];
@@ -152,38 +151,4 @@ export const GALLERY_IMAGES = [
   { url: 'https://i.imgur.com/0qSPOOO.png', category: 'Casamentos', title: 'Decoração Pacote Label - Cerimônia Premium' },
   { url: 'https://i.imgur.com/KerTmNA.png', category: 'Especiais', title: 'Decoração Pacote Label - Lounge Clássico' },
   { url: 'https://i.imgur.com/rMAVKle.png', category: 'Festas', title: 'Decoração Pacote Label - Detalhes de Brilho' },
-  { url: 'https://i.imgur.com/cGXKynd.png', category: 'Casamentos', title: 'Decoração Pacote Label - Gazebo dos Noivos' },
-  { url: 'https://i.imgur.com/FtlEMV2.png', category: 'Especiais', title: 'Decoração Pacote Label - Mesa Redonda' },
-  { url: 'https://i.imgur.com/1Wn9Pot.png', category: 'Festas', title: 'Decoração Pacote Label - Lounge Minimalista' },
-  { url: 'https://i.imgur.com/cjPUAe5.png', category: 'Gastronomia', title: 'Banquete & Buffet Estilo Ava' },
-
-  { url: 'https://i.imgur.com/XMykzEf.png', category: 'Casamentos', title: 'Decoração Clássica de Casamento' },
-  { url: 'https://i.imgur.com/nLxHquT.png', category: 'Especiais', title: 'Cenário de Entrada Monumental' },
-  { url: 'https://i.imgur.com/LmutJhO.png', category: 'Corporativo', title: 'Detalhes em Cristais e Ouro' },
-  { url: 'https://i.imgur.com/uqMWmbx.png', category: 'Casamentos', title: 'Ambiente de Recepção Premium' },
-  { url: 'https://i.imgur.com/qM3JIw0.png', category: 'Corporativo', title: 'Banquete de Gala' },
-  { url: 'https://i.imgur.com/U1FRqX5.png', category: 'Casamentos', title: 'Teto Floral de Luxo' },
-  
-  { url: 'https://i.imgur.com/kWySv7R.png', category: 'Especiais', title: 'Arte em Detalhes' },
-  { url: 'https://i.imgur.com/sinppBQ.png', category: 'Casamentos', title: 'Cerimonial de Luxo' },
-  { url: 'https://i.imgur.com/LfzqD2U.png', category: 'Festas', title: 'Ambiente Festivo Ava' },
-  { url: 'https://i.imgur.com/mrkRvmo.png', category: 'Especiais', title: 'Curadoria de Cenários' },
-  { url: 'https://i.imgur.com/51XbGRs.png', category: 'Casamentos', title: 'Altar Monumental' },
-  { url: 'https://i.imgur.com/ukMjTPt.png', category: 'Corporativo', title: 'Recepção Executiva' },
-  { url: 'https://i.imgur.com/wY7xj4F.png', category: 'Festas', title: 'Noite de Gala Ava' },
-  
-  { url: 'https://i.imgur.com/tZNLdZc.png', category: 'Gastronomia', title: 'Apresentação de Pratos Quentes' },
-  { url: 'https://i.imgur.com/ilVfZFK.png', category: 'Gastronomia', title: 'Buffet de Doces & Sobremesas' },
-  { url: 'https://i.imgur.com/VYPtZXG.png', category: 'Gastronomia', title: 'Entradas Gourmet Ava' },
-  { url: 'https://i.imgur.com/xo2cSQz.png', category: 'Gastronomia', title: 'Serviço de Banquetes Luxo' },
-  { url: 'https://i.imgur.com/xo2cSQz.png', category: 'Gastronomia', title: 'Experiência Gastronômica Ava' },
-
-  { url: 'https://i.imgur.com/scHAorL.png', category: 'Especiais', title: 'Arte Visual Ava' },
-  { url: 'https://i.imgur.com/HLBViFX.png', category: 'Corporativo', title: 'Espaço de Eventos' }
-];
-
-export const SOCIAL_LINKS = {
-  instagram: 'https://www.instagram.com/avaeventos.s',
-  facebook: 'https://www.facebook.com/share/1FdLjBMALU/',
-  whatsapp: 'https://wa.me/244948757808'
-};
+  { url: 'https://i.imgur.com/cGXKynd.png', category: 'Casamentos', title: 'Decoração Pacote Label -
