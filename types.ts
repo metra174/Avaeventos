1500198dcc0b87b6eee94807e2360b0cb1049257
Ageu Manuel <140927063+metra174@users.noreply.github.com>
2026-06-21 06:05:21 +0100

    R3
    Q

--- types.ts
+++ types.ts
@@ -1,30 +1,12 @@
-{
-  "compilerOptions": {
-    "target": "ES2022",
-    "experimentalDecorators": true,
-    "useDefineForClassFields": false,
-    "module": "ESNext",
-    "lib": [
-      "ES2022",
-      "DOM",
-      "DOM.Iterable"
-    ],
-    "skipLibCheck": true,
-    "types": [
-      "node"
-    ],
-    "moduleResolution": "bundler",
-    "isolatedModules": true,
-    "moduleDetection": "force",
-    "allowJs": true,
-    "jsx": "react-jsx",
-    "paths": {
-      "@/*": [
-        "./*"
-      ]
-    },
-    "allowImportingTsExtensions": true,
-    "noEmit": true
-  }
-}
+export interface Package {
+  id: string;
+  name: string;
+  price: string;
+  currency: string;
+  color: string;
+  tagline: string;
+  image: string;
+  images?: string[]; // O "?" torna as imagens adicionais opcionais para pacotes que não têm
+  location: string;
+  features: string[];
 }
